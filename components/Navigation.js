35c7febc2769bc8aaa129cfa6befa803b7cc103a
AnikHaque <ekramul15-811@diu.edu.bd>
2022-08-14 12:01:08 -0500

    adding routes

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar,Container, Nav, NavDropdown } from 'react-bootstrap';
+import Link from 'next/link';
 
 const Navigation = () => {
     return ( 
@@ -10,19 +11,15 @@ const Navigation = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link as={Link} href='/'>
+           Home
+            </Nav.Link>
+            <Nav.Link as={Link} href='/about'>
+           About
+            </Nav.Link>
+            
+            
+            
           </Nav>
         </Navbar.Collapse>
       </Container>
